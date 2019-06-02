afac04f67a218632bccc9cc24e21809b0ebc3e43
krypto639 <kyle.whyte639@gmail.com>
2019-06-02 22:50:24 +1200

    v1_Roughs
    still rough but have a working nav, working menu, working destination explore and a working mobile list page.
    
    PAGES
    list mobile and tablet
    history explore
    history list
    events explore
    events list
    activities explore
    activities list
    
    EDITS
    -all photos compressed
    -all photos have accurate alt tags
    -add brand identity to nav side menu
    -code no longer in use or relevant deleted to make scanning easier

---
+++ _/components/destination/app.js
@@ -0,0 +1,38 @@
+var map = document.querySelector('#map')
+var paths = map.querySelectorAll('.map__image path')
+var links = map.querySelectorAll('.map__list li')
+
+//Polyfill forEach
+if (NodeList.prototype.forEach === undefined) {
+    NodeList.prototype.forEach = function (callback) {
+        [].forEach.call(this, callback)
+    }
+}
+
+var activeArea = function (id) {
+    map.querySelectorAll('.is-active').forEach(function (item) {
+        item.classList.remove('is-active')
+    })
+    if(id!==undefined)
+    {document.querySelector('#list-' + id).classList.add('is-active');
+    document.querySelector('#region-' + id).classList.add('is-active');}
+
+}
+
+paths.forEach(function (path) {
+    path.addEventListener('mouseenter', function (e) {
+        var id = this.id.replace('region-', '')
+        activeArea(id)
+    })
+})
+
+links.forEach(function (link) {
+    link.addEventListener('mouseenter', function () {
+        var id = this.id.replace('list-', '')
+        activeArea(id)
+    })
+})
+
+map.addEventListener('mouseleave', function(){
+    activeArea()
+})
